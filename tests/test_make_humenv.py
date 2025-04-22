f7c2af31f400c82119cd3ee44147ebb415431f03
MateuszGuzek <matguzek@meta.com>
2025-04-22 11:35:20 +0000

    Improve test

--- tests/test_make_humenv.py
+++ tests/test_make_humenv.py
@@ -6,25 +6,34 @@
 import gymnasium
 import numpy as np
 import pytest
+import typing as tp
 
 from humenv import make_humenv
 
 
 @pytest.mark.parametrize("num_envs", [2, 5])
+@pytest.mark.parametrize("vectorization_mode", ["sync", "async"])
 @pytest.mark.parametrize("seed", [None, 1])
 @pytest.mark.parametrize("state_init", ["Default", "Fall"])
-def test_make_humenv(num_envs, seed, state_init):
+def test_make_humenv(
+    num_envs: int,
+    state_init: str,
+    vectorization_mode: str,
+    seed: tp.Optional[int],
+):
     penv, _ = make_humenv(
         num_envs=num_envs,
-        vectorization_mode="sync",
+        vectorization_mode=vectorization_mode,
         motions=None,
         motion_base_path=None,
         wrappers=[gymnasium.wrappers.FlattenObservation],
         state_init=state_init,
     )
-    for k in range(2):
+    reset_no = 2
+    step_no = 50
+    for k in range(reset_no):
         observations, infos = penv.reset(seed=seed)
-        for j in range(50):
+        for j in range(step_no):
             for i in range(len(observations) - 1):
                 if state_init == "Default":
                     np.testing.assert_allclose(observations[i], observations[i + 1])
